045c0ff734f81f6dde5e11daa72f4f60ea0f957a
cendekia <cendekia@sonar.id>
2024-08-15 08:25:08 +0700

    fix(snapsnot): memory leak

--- src/index.ts
+++ src/index.ts
@@ -23,57 +23,66 @@ app.get("/pdf", async (request, response) => {
   const filename: string = request.query.filename as string || "document";
   const download: boolean = request.query.download === "true";
 
-  // Create an instance of the chrome browser
-  // But disable headless mode !
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
+  let browser;
 
-  // Create a new page
-  const webPage = await browser.newPage();
-
-  // Configure the navigation timeout
-  await webPage.setDefaultNavigationTimeout(0);
-
-  // Navigate to website
-  await webPage
-    .goto(url, {
-      waitUntil: "networkidle0",
-    })
-    .catch((err) => console.log("error loading url", err))
-
-  await webPage.waitForNetworkIdle();
-
-  // wait by blocking execution flow
-
-  await new Promise((resolve) => setTimeout(resolve, 5000))
-
-  const pdf = await webPage.pdf({
-    printBackground: true,
-    format: formats.includes(format) ? format as PaperFormat : "Tabloid",
-    displayHeaderFooter: showPageNumber,
-    footerTemplate: showPageNumber ? `<div style="text-align: right;width: 297mm;font-size: 12px;"><span style="margin-right: 1cm"><span class="pageNumber"></span>/<span class="totalPages"></span></span></div>` : "",
-    margin: {
-      top: "20px",
-      bottom: showPageNumber ? "40px" : "20px",
-      left: "20px",
-      right: "20px",
-    },
-  })
-
-  await browser.close()
-
-  if (download) {
-    response.writeHead(200, {
-      "Content-Type": "application/pdf",
-      "Content-Disposition": `attachment; filename=${filename}.pdf`,
-      "Content-Length": pdf.length,
+  try {
+    // Create an instance of the chrome browser
+    // But disable headless mode !
+    browser = await puppeteer.launch({
+      headless: true,
     });
-    
-    response.end(pdf);
-  } else {
-    response.contentType("application/pdf")
-    response.send(pdf)
+
+    // Create a new page
+    const webPage = await browser.newPage();
+
+    // Configure the navigation timeout
+    await webPage.setDefaultNavigationTimeout(0);
+
+    // Navigate to website
+    await webPage
+      .goto(url, {
+        waitUntil: "networkidle0",
+      })
+      .catch((err) => console.log("error loading url", err))
+
+    await webPage.waitForNetworkIdle();
+
+    // wait by blocking execution flow
+
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+
+    const pdf = await webPage.pdf({
+      printBackground: true,
+      format: formats.includes(format) ? format as PaperFormat : "Tabloid",
+      displayHeaderFooter: showPageNumber,
+      footerTemplate: showPageNumber ? `<div style="text-align: right;width: 297mm;font-size: 12px;"><span style="margin-right: 1cm"><span class="pageNumber"></span>/<span class="totalPages"></span></span></div>` : "",
+      margin: {
+        top: "20px",
+        bottom: showPageNumber ? "40px" : "20px",
+        left: "20px",
+        right: "20px",
+      },
+    })
+
+    if (download) {
+      response.writeHead(200, {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename=${filename}.pdf`,
+        "Content-Length": pdf.length,
+      });
+      
+      response.end(pdf);
+    } else {
+      response.contentType("application/pdf")
+      response.send(pdf)
+    }
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ message: "Internal Server Error" })
+  } finally {
+    if (browser) {
+      await browser.close()
+    }
   }
 })
 
@@ -82,14 +91,16 @@ app.get("/image", async (request, response) => {
   const type: string = request.query.type as string
   const types = ["png", "jpeg", "webp"]
 
-  if (types.includes(type) === false) {
-    response
-      .status(400)
-      .json({ message: "Supported types are png, jpeg or webp" })
-  } else {
+  if (!types.includes(type)) {
+    response.status(400).json({ message: "Supported types are png, jpeg or webp" });
+    return;
+  }
+
+  let browser;
+  try {
     // Create an instance of the chrome browser
     // But disable headless mode !
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
       headless: true,
     })
 
@@ -97,7 +108,7 @@ app.get("/image", async (request, response) => {
     const webPage = await browser.newPage()
 
     // Configure the navigation timeout
-    await webPage.setDefaultNavigationTimeout(0)
+    webPage.setDefaultNavigationTimeout(0)
 
     // Navigate to website
     await webPage
@@ -138,16 +149,24 @@ app.get("/image", async (request, response) => {
       y: boundingBox.y,
     }
 
+    // delay to allow the page to render
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+
     const png = await webPage.screenshot(<ScreenshotOptions>{
       captureBeyondViewport: true,
       fromSurface: true,
       clip: boundingBox,
     })
 
-    await browser.close()
-
     response.contentType("image/png")
     response.send(png)
+  } catch (error) {
+    console.error("Error generating image:", error);
+    response.status(500).send("Internal Server Error");
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 })
 
